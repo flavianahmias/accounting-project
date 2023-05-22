8cbc9a1c4e0ac40217d53532bca326755ff2f544
Heleny Flavia <flaviaanahmias@gmail.com>
2023-05-22 10:19:28 -0400

    feat: add subtitle in sidebar

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -4,9 +4,23 @@ import SvgUser from '../../assets/users-solid.svg';
 import SvgBagMoney from '../../assets/sack-dollar-solid.svg';
 import SvgLogo from '../../assets/logo.svg';
 import SvgLogoSymbol from '../../assets/logo-symbol.svg';
+import { useEffect, useState } from 'react';
 
 export default function Sidebar() {
-  const location = window?.location.pathname.replace('/', '');
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    const getPathname = window.location.pathname.replace('/', '');
+    setLocation(getPathname);
+    changeSelectedPageStyle(getPathname);
+  });
+
+  const handleClickOption = (page: string) => {
+    if (page !== location) {
+      setLocation(page);
+      changeSelectedPageStyle(page);
+    }
+  };
 
   const changeSelectedPageStyle = (pageSelected: string) => {
     if (pageSelected === location) return 'selected';
@@ -17,13 +31,22 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="logo">
         <SvgLogo />
+        <p>Contabilidade</p>
       </div>
-      <Link href="/transactions" className="link" onClick={() => {}}>
+      <Link
+        href="/transactions"
+        className="link"
+        onClick={() => handleClickOption('transactions')}
+      >
         <button className={changeSelectedPageStyle('transactions')}>
           <SvgBagMoney className="sidebarSVG" /> Transações
         </button>
       </Link>
-      <Link href="/users" className="link">
+      <Link
+        href="/users"
+        className="link"
+        onClick={() => handleClickOption('users')}
+      >
         <button className={changeSelectedPageStyle('users')}>
           <SvgUser className="sidebarSVG" /> Usuarios
         </button>
