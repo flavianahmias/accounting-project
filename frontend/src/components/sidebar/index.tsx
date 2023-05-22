ee7a04df1a6ec0b5728feb601d2fe5e53dd2715f
Heleny Flavia <flaviaanahmias@gmail.com>
2023-05-22 00:28:09 -0400

    fix: ajust logic sidebar

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -4,7 +4,7 @@ import SvgUser from '../../assets/users-solid.svg';
 import SvgBagMoney from '../../assets/sack-dollar-solid.svg';
 
 export default function Sidebar() {
-  const location = window.location.pathname.replace('/', '');
+  const location = window?.location.pathname.replace('/', '');
 
   const changeSelectedPageStyle = (pageSelected: string) => {
     if (pageSelected === location) return 'selected';
@@ -13,7 +13,7 @@ export default function Sidebar() {
 
   return (
     <div className="sidebar">
-      <Link href="/transactions" className="link">
+      <Link href="/transactions" className="link" onClick={() => {}}>
         <button className={changeSelectedPageStyle('transactions')}>
           <SvgBagMoney className="sidebarSVG" /> Transações
         </button>
