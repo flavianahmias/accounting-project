d59a8bcf265171fbb79438c74ee005d8fd89787d
Heleny Flavia <flaviaanahmias@gmail.com>
2023-05-22 09:59:38 -0400

    feat: add hubla logo in sidebar

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -2,6 +2,8 @@ import './styles.scss';
 import Link from 'next/link';
 import SvgUser from '../../assets/users-solid.svg';
 import SvgBagMoney from '../../assets/sack-dollar-solid.svg';
+import SvgLogo from '../../assets/logo.svg';
+import SvgLogoSymbol from '../../assets/logo-symbol.svg';
 
 export default function Sidebar() {
   const location = window?.location.pathname.replace('/', '');
@@ -13,6 +15,9 @@ export default function Sidebar() {
 
   return (
     <div className="sidebar">
+      <div className="logo">
+        <SvgLogo />
+      </div>
       <Link href="/transactions" className="link" onClick={() => {}}>
         <button className={changeSelectedPageStyle('transactions')}>
           <SvgBagMoney className="sidebarSVG" /> Transações
